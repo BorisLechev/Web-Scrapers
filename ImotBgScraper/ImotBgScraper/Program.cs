e1b3c2d92364b118437c8a489aaaa1b016889e78
BorisLechev <b.k.lechev@gmail.com>
2022-05-07 21:41:50 +0300

    Add code for properties filtration by Price, Year, Floor, TotalFloors.

--- ImotBgScraper/ImotBgScraper/Program.cs
+++ ImotBgScraper/ImotBgScraper/Program.cs
@@ -15,3 +15,11 @@ var properties = await gather.GatherDataAsync(10, 1000);
 
 //using var csvWriter = new CsvWriter(new StreamWriter(File.OpenWrite($"imot.bg-raw-data-{DateTime.Now:yyyy-MM-dd}.csv"), Encoding.UTF8), CultureInfo.CurrentCulture);
 //csvWriter.WriteRecords(properties);
+
+// 13657 records in imot.bg-2022-04-25.csv
+//var filteredProperties = properties
+//                        .Where(p => p.Price > 0 && p.Year > 0 && p.Floor > 0 && p.TotalFloors > 0)
+//                        .ToList();
+
+//using var csvWriter = new CsvWriter(new StreamWriter(File.OpenWrite($"imot.bg-{DateTime.Now:yyyy-MM-dd}.csv"), Encoding.UTF8), CultureInfo.CurrentCulture);
+//await csvWriter.WriteRecordsAsync(filteredProperties);
