9628d97d9634cda1ff895107066348ab5bfc30ad
BorisLechev <b.k.lechev@gmail.com>
2022-05-07 21:06:41 +0300

    Change name of the file in the comment.

--- ImotBgScraper/ImotBgScraper/Program.cs
+++ ImotBgScraper/ImotBgScraper/Program.cs
@@ -8,7 +8,7 @@ using System.Text;
 var gather = new ImotBgDataGather();
 var properties = await gather.GatherDataAsync(10, 1000);
 
-// 17341 records in imot.bg-2022-04-25.csv
+// 17341 records in imot.bg-raw-data-2022-04-25.csv
 //File.WriteAllText(
 //   $"imot.bg-raw-data-{DateTime.Now:yyyy-MM-dd}.json",
 //   JsonConvert.SerializeObject(properties, Formatting.Indented));
